e7b68f410a0ad32fd27a88819237166ac2887361
Zachary Shold <sholdzb@gmail.com>
2022-06-22 20:39:19 -0500

    Time: 99 ms (67.55%), Space: 42.2 MB (80.69%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -7,11 +7,12 @@ var twoSum = function(nums, target) {
     let result = [];
     for (let i = 0; i < nums.length; i++) {
         let currentTarget = target - nums[i];
-        let match = nums.indexOf(currentTarget)
-        if (match > -1 && match !== i) {
-            result = [i, match]
-            break;
+        for (let n = i+1; n < nums.length; n++) {
+            const num = nums[n];
+            if (currentTarget === num) {
+                return [i, n]
+            }
         }
     }
-    return result
+    return [];
 };
